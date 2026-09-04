dfd320507726135f56f5af576870b9ceaa5a3da7
ABHINAV SINGH <abhi.prep.24@gmail.com>
2026-09-04 22:33:02 +0530

    Time: 0 ms (100%), Space: 23.8 MB (82.73%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -2,18 +2,19 @@ class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
         int n=nums.size();
-        vector<int>temp;
+        int j=-1;
         for(int i=0;i<n;i++){
-            if(nums[i]!=0){
-                temp.push_back(nums[i]);
+            if(nums[i]==0){
+                j=i;
+                break;
             }
         }
-        int z=temp.size();
-        for(int i=0;i<z;i++){
-            nums[i]=temp[i];
-        }
-        for(int i=z;i<n;i++){
-            nums[i]=0;
+        if(j==-1)return;
+        for(int i=j+1;i<n;i++){
+            if(nums[i]!=0){
+                swap(nums[i],nums[j]);
+                j++;
+            }
         }
     }
 };
